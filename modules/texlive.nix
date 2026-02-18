0a24884e477243a88808797e3800241bd6f7fc77
Yang, Bo <yang-bo@yang-bo.com>
2026-02-18 08:48:50 +0000

    feat: add package-arxiv for automated tarball creation of submission files

--- modules/texlive.nix
+++ modules/texlive.nix
@@ -1,40 +1,49 @@
-{ ... }:
-{
-  imports = [
-    ./dev.nix
-  ];
-  partitions.dev.module.perSystem = { pkgs, ... }: {
-    ml-ops.devcontainer.devenvShellModule = {
-      packages = [
-        pkgs.tex-fmt
-      ];
-      languages = {
-        texlive = {
-          enable = true;
-          packages = [
-            "scheme-medium"
-            "cjk"
-            "xpinyin"
-            "latexmk"
-            # acmart dependencies not in scheme-medium
-            "xstring"
-            "totpages"
-            "environ"
-            "trimspaces"
-            "ncctools"
-            "comment"
-            "pbalance"
-            "libertine"
-            "inconsolata"
-            "newtx"
-            "hyperxmp"
-            "ifmtarg"
-            "draftwatermark"
-            "preprint"
-            "tex-gyre"
-          ];
+{ ... }: {
+  imports = [ ./dev.nix ];
+  partitions.dev.module.perSystem = { pkgs, ... }:
+    let
+      package-arxiv = pkgs.writeShellApplication {
+        name = "package-arxiv";
+        runtimeInputs = [ pkgs.gnutar ];
+        text = ''
+          tar -czvf arxiv-submission.tar.gz \
+            -C mixin-calculus \
+            *.tex \
+            *.bib \
+            *.bst \
+            *.cls
+        '';
+      };
+    in {
+      ml-ops.devcontainer.devenvShellModule = {
+        packages = [ pkgs.tex-fmt package-arxiv ];
+        languages = {
+          texlive = {
+            enable = true;
+            packages = [
+              "scheme-medium"
+              "cjk"
+              "xpinyin"
+              "latexmk"
+              # acmart dependencies not in scheme-medium
+              "xstring"
+              "totpages"
+              "environ"
+              "trimspaces"
+              "ncctools"
+              "comment"
+              "pbalance"
+              "libertine"
+              "inconsolata"
+              "newtx"
+              "hyperxmp"
+              "ifmtarg"
+              "draftwatermark"
+              "preprint"
+              "tex-gyre"
+            ];
+          };
         };
       };
     };
-  };
 }
